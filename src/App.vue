9be73619513cc7c03e68d90617f224f0a07078bb
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-28 20:41:50 -0500

    [sm] got product data displaying on the screen, very cool

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-4 p-4 shadaow border">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div>{{ product }}</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
