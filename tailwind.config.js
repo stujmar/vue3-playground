261ab6ed39d74993c897316a48f2e551c6a05abd
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-28 19:39:40 -0500

    [sm] got tailwind to purge actually finally

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+    './src/**/*.js',
+    './src/**/*.vue',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
