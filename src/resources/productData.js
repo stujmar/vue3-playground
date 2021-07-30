ef0234904d8110bf6313f8a3505617bb5aa2551c
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-29 21:51:14 -0500

    [sm] added a second product in the product data as well as a inStock boolean

--- src/resources/productData.js
+++ src/resources/productData.js
@@ -1,6 +1,12 @@
 const productData = [ {
     name: "Sponge Man",
-    image: './assets/pikabob.png'
+    image: './assets/pikabob.png',
+    inStock: true,
+    },
+    {
+    name: "Cat Thing",
+    image: './assets/meowthfield.png',
+    inStock: true,
     },
 ];
 
