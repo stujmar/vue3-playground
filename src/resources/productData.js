1b9a0bccebcabe6d314f42d16390e71b0aca5e62
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-30 20:53:18 -0500

    [sm] added array of detail strings to each product

--- src/resources/productData.js
+++ src/resources/productData.js
@@ -2,25 +2,29 @@ const productData = [ {
     name: "Sponge Man",
     image: './assets/pikabob.png',
     inventory: 20,
-    onSale: false
+    onSale: false,
+    details: ['Yellow', 'Soft', 'Absorbent'],
     },
     {
     name: "Cat Thing",
     image: './assets/meowthfield.png',
     inventory: 0,
-    onSale: false
+    onSale: false,
+    details: ['Mondays', 'Hairballs', 'Frisky'],
     },
     {
     name: "Turtler",
     image: './assets/squirtle_turtle.png',
     inventory: 6,
-    onSale: false
+    onSale: false,
+    details: ['Shelled', 'Pizza', 'Souped'],
     },
     {
     name: "Door Knob",
     image: './assets/eeyore.png',
     inventory: 25,
-    onSale: true
+    onSale: true,
+    details: ['Horsey', 'Sad', 'Quadripedal'],
     },
 ];
 
