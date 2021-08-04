3fd9ac4a1aa463d117e61d0c4a098d2df1fab179
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-03 22:28:15 -0500

    [sm] build individual components for Shopping Cart and Product Display

---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>This is a Single Product</div>
+</template>
+
+<script>
+    export default {
+        name: "ProductDisplay",
+        data () {
+            return {
+                productData: {}
+            }
+        }
+    }
+</script>
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>This is the Shopping Cart</div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+
