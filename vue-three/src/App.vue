b42644cb11c40cc5b3b03b2d81599fa7a5e3e9f9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-04-19 21:36:56 -0500

    [sm] broke repo into vue3 and nuxt3 projects, started a linkedin learning mastery course

---
+++ vue-three/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="msg"/>
+  <button :onClick="toggleVisibility" >Check if Vue is working</button>
+  <div v-if="isVisible">
+    <p>It is!</p>
+  </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      msg: 'Welcome to My Vue.js App',
+      isVisible: false
+    }
+  },
+  methods: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
