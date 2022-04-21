5f1322eb2a0f9da0f8065d1092d72f1afe4bdfb6
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-04-20 23:17:15 -0500

    [sm] started show vs if in templating exerize

---
+++ vue-three/src/components/Show.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <p>This is the app.vue</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
