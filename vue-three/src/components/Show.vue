3b8270af6d32f489ae750d7130f7e96edd4ce2ba
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-04-22 22:10:58 -0500

    [sm] setup a component to use composition api methods but yet to work

--- vue-three/src/components/Show.vue
+++ vue-three/src/components/Show.vue
@@ -3,6 +3,9 @@
     <h1>Hello World</h1>
     <p>This is the app.vue</p>
     <div class="pb-2">{{msg}}</div>
+    <!-- button to toggle visibility -->
+    <button @click="toggle()">Check if  is on</button>
+    <div>{{ isOn }}</div>
   </div>
 </template>
 
@@ -12,8 +15,15 @@
 export default ({
   setup() {
     const msg = 'Hello from the composition API'
+    let isOn = false
+    // functio to toggle the state
+    const toggle = () => {
+      isOn = !isOn
+    }
     return {
-      msg
+      msg,
+      isOn,
+      toggle
     }
   },
 })
