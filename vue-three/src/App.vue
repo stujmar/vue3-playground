54d7871d86bd17023cfd0c2a0fda90e8c6e0f651
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-04-25 22:01:08 -0500

    [sm] phew finially got option api nonsense working in the darn tutorial example

--- vue-three/src/App.vue
+++ vue-three/src/App.vue
@@ -2,11 +2,15 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Flashcard from './components/Flashcard.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" class="mx-auto" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <img alt="Vue logo" class="mx-auto" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <Flashcard />
+  </div>
 </template>
 
 <style>
---
+++ vue-three/src/components/Flashcard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 v-text="msg"></h1>
+    TEST
+  </div>
+</template>
+
+<script>
+import cards from '../assets/data.json';
+
+  export default {
+    data() {
+      return {
+        msg: "hello from Flashcard.vue",
+        cards: cards
+      }
+    }
+  }
+</script>
