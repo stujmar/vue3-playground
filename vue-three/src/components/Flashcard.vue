69f9e00a71abc6cb01e2a5ffcfd86957c5da66f0
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-04-26 21:54:23 -0500

    [sm] got card data to render in a v-for

--- vue-three/src/components/Flashcard.vue
+++ vue-three/src/components/Flashcard.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h1 v-text="msg"></h1>
-    TEST
+    <div v-for="card in cards" :key="card.id" class="border w-max mx-auto m-2 px-2">
+      <div v-text="card.name"></div>
+    </div>
   </div>
 </template>
 
