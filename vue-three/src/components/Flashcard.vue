6c9a926eee7dc7c805d6f2f9f4108c9a57df2069
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-04-27 21:51:50 -0500

    [sm] built out flipped boolean logic and display of single card

--- vue-three/src/components/Flashcard.vue
+++ vue-three/src/components/Flashcard.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1 v-text="msg"></h1>
+    <div class="border my10">
+      <h2 v-text="flipped ? '' : cards[0].name"></h2>
+    </div>
     <div v-for="card in cards" :key="card.id" class="border w-max mx-auto m-2 px-2">
       <div v-text="card.name"></div>
     </div>
@@ -14,7 +17,8 @@ import cards from '../assets/data.json';
     data() {
       return {
         msg: "hello from Flashcard.vue",
-        cards: cards
+        cards: cards,
+        flipped: false
       }
     }
   }
