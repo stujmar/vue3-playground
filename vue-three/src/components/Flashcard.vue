83167adc3becaf0059dbb2ab1d041329d0be6137
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-05-04 22:20:18 -0500

    [sm] got buttons to navigate cards forward and back and flip the cards

--- vue-three/src/components/Flashcard.vue
+++ vue-three/src/components/Flashcard.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div class="border m-4 font-bold text-xl">{{ cards[index].definition }}</div>
+    <div class="border m-4 font-bold text-xl">{{ flipped ? cards[index].name : cards[index].definition }}</div>
     <div class="flex gap-4 w-max mt-4 mx-auto">
-      <button class="font-bold" v-text="`<=`"></button>
+      <button class="font-bold" v-text="`<=`" @click="indexBack"></button>
       <button type="button" class="font-bold" v-text="`flip`" @click="flipCard"></button>
-      <button class="font-bold" v-text="`=>`"></button>
+      <button class="font-bold" v-text="`=>`" @click="indexForward"></button>
     </div>
  </div>
 </template>
@@ -28,7 +28,13 @@ import cards from '../assets/data.json';
       flipCard() {
         console.log("flip");
         this.flipped = !this.flipped;
-      }
+      },
+      indexForward() {
+       this.index < this.cards.length - 1 ? this.index++ : this.index = this.index;
+      },
+      indexBack() {
+       this.index > 0 ? this.index-- : this.index = this.index;
+      },
     }
   }
 </script>
