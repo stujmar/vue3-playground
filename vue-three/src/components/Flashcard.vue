ef76028df317f70c6de6b4ec22914d57942b8f5d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-05-05 21:50:22 -0500

    [sm] got flipped logic correct, text centered on card and card unfliping when navigating to a new card

--- vue-three/src/components/Flashcard.vue
+++ vue-three/src/components/Flashcard.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <div class="border m-4 font-bold text-xl">{{ flipped ? cards[index].name : cards[index].definition }}</div>
-    <div class="flex gap-4 w-max mt-4 mx-auto">
+    <div class="relative border m-4 font-bold h-40 w-90 text-xl bg-gray-100 shadow-md">
+      <div class="absolute inset-0 m-auto h-min">
+        {{ flipped ? cards[index].definition : cards[index].name }}
+      </div>
+    </div>
+    <div class="flex gap-4 w-max h-min mt-4 mx-auto">
       <button class="font-bold" v-text="`<=`" @click="indexBack"></button>
       <button type="button" class="font-bold" v-text="`flip`" @click="flipCard"></button>
       <button class="font-bold" v-text="`=>`" @click="indexForward"></button>
@@ -30,10 +34,12 @@ import cards from '../assets/data.json';
         this.flipped = !this.flipped;
       },
       indexForward() {
+        this.flipped  = false;
        this.index < this.cards.length - 1 ? this.index++ : this.index = this.index;
       },
       indexBack() {
-       this.index > 0 ? this.index-- : this.index = this.index;
+        this.flipped  = false;
+        this.index > 0 ? this.index-- : this.index = this.index;
       },
     }
   }
