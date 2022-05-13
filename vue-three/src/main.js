49911b0f9bb5b5a6d39995675b54558af0393770
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-05-12 21:44:49 -0500

    [sm] moved the router code to its own dir and file

--- vue-three/src/main.js
+++ vue-three/src/main.js
@@ -1,14 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import {createRouter, createWebHistory} from 'vue-router'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', name: 'Home', component: () => import('./views/Home.vue') },
-    { path: '/about', name: 'About', component: () => import('./views/About.vue') },
-  ],
-})
+import router from './router'
 
 createApp(App).use(router).mount('#app');
